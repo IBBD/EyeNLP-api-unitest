d60285c56bca70484cd37ade4a49d78a425449e8
alex cai <caiyingyao@ibbd.net>
2017-05-27 14:21:50 +0800

    classify

--- classify.py
+++ classify.py
@@ -15,8 +15,16 @@ def cli(host):
 
     print("-"*40)
     contents = []
+    for i in range(2):
+        contents.append('广州迪奥信息科技有限公司是一家从事大数据研发的公司, 地址是广州市天河区华观路新塘田头岗二路一横街4号B栋3楼. %d' % i)
+
+    r = requests.post(url, json={"contents": contents}).json()
+    pp.pprint(r)
+
+    print("-"*40)
+    contents = []
     for i in range(1000):
-        contents.append('广州迪奥信息科技有限公司是一家从事大数据研发的公司, 地址是广州市天河区华观路新塘田头岗二路一横街4号B栋3楼.')
+        contents.append('广州迪奥信息科技有限公司是一家从事大数据研发的公司, 地址是广州市天河区华观路新塘田头岗二路一横街4号B栋3楼. %d' % i)
 
     r = requests.post(url, json={"contents": contents}).json()
     pp.pprint(r['run_time'])
